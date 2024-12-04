692d1e112225712af40b7087ab9f28770ec26a0a
timeflew <136106767+twfly104@users.noreply.github.com>
2024-12-04 23:16:32 +0800

    Create 10035.cpp

---
+++ loj/10035.cpp
@@ -0,0 +1,47 @@
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define timeflew
+#ifdef timeflew
+#define dbg(LINE...) cerr << "[" << #LINE << "] : ", dout(LINE)
+void dout() {cerr << '\n';}
+template<typename T, typename...U>
+void dout(T t, U...u) {cerr << t << (sizeof... (u) ? ", ":""), dout(u...);}
+#else
+#define dbg(...) 7122
+#endif
+
+#define pb push_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define F first
+#define S second
+#define sz(x) (int)(x.size())
+#define FOR(i, n) for (int i = 0; i < (n); i++)
+#define FOR1(i, a, b) for (int i = (a); i <= (b); i++)
+
+const int mxN = 1e6;
+int p[mxN];
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0);
+	string s;
+	while (cin >> s) {
+		if (s == ".") break;
+		for (int i = 1, j = 0; i < sz(s); i++) {
+			while (j && s[i] != s[j]) {
+				j = p[j];
+			}
+			p[i] = j;
+      if (s[i] == s[j]) j++;
+		}
+		if (sz(s) % (sz(s) - p[sz(s) - 1] - 1) == 0 && (sz(s) - p[sz(s) - 1] - 1) != 0) {
+			cout << sz(s) / (sz(s) - p[sz(s) - 1] - 1) << "\n";
+		} else {
+			cout << "1\n";
+		}
+	}
+	return 0;
+}
