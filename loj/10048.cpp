3fcc2b5fcd5d61e0c9365a2bd4b4c1f39c8a466d
timeflew <136106767+twfly104@users.noreply.github.com>
2024-12-12 23:38:34 +0800

    Update 10048.cpp

--- loj/10048.cpp
+++ loj/10048.cpp
@@ -27,39 +27,39 @@ int p[mxN];
 string s, s1;
 
 void kmp() {
-	for (int i = 1, j = 0; i < sz(s1); i++) {
-		while (j && s1[i] != s1[j]) j = p[j - 1];
-		if (s1[i] == s1[j]) j++;
-		p[i] = j;
-	}
+    for (int i = 1, j = 0; i < sz(s1); i++) {
+        while (j && s1[i] != s1[j]) j = p[j - 1];
+        if (s1[i] == s1[j]) j++;
+        p[i] = j;
+    }
 }
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(0);
-	cin >> s >> s1;
-	stack<int> stk;
-	stack<char> ans;
-	kmp();
-	for (int i = 0, j = 0; i < sz(s); i++) {
-		while (j && s[i] != s1[j]) j = p[j - 1];
-		if (s[i] == s1[j]) j++;
-		stk.push(j);
-		ans.push(s[i]);
-		if (j == sz(s1)) {
-			int t = j;
-			while (t--) stk.pop(), ans.pop();
-			if (sz(stk)) //!!!, add this condition or get RE
-				j = stk.top();
-			else
-				j = 0;
-		}
-	}
-	string ans1;
-	while (sz(ans)) {
-		ans1 += ans.top();
-		ans.pop();
-	}
-	reverse(ans1.begin(), ans1.end());
-	cout << ans1;
-	return 0;
+    ios::sync_with_stdio(false); cin.tie(0);
+    cin >> s >> s1;
+    stack<int> stk;
+    stack<char> ans;
+    kmp();
+    for (int i = 0, j = 0; i < sz(s); i++) {
+        while (j && s[i] != s1[j]) j = p[j - 1];
+        if (s[i] == s1[j]) j++;
+        stk.push(j);
+        ans.push(s[i]);
+        if (j == sz(s1)) {
+            int t = j;
+            while (t--) stk.pop(), ans.pop();
+            if (sz(stk)) //!!!, add this condition or get RE
+                j = stk.top();
+            else
+                j = 0;
+        }
+    }
+    string ans1;
+    while (sz(ans)) {
+        ans1 += ans.top();
+        ans.pop();
+    }
+    reverse(ans1.begin(), ans1.end());
+    cout << ans1;
+    return 0;
 }
