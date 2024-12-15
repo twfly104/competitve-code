6ff640b7fd3fddf2f1ebe06e0044772a3ab98213
timeflew <136106767+twfly104@users.noreply.github.com>
2024-12-15 09:01:50 +0800

    Update 10049.cpp

--- loj/10049.cpp
+++ loj/10049.cpp
@@ -27,36 +27,36 @@ int ch[mxN + 1][10], total;
 bool b[mxN + 1], flag;
 
 struct trie {
-	void init() {
-		memset(ch, -1, sizeof(ch));
-		memset(b, false, sizeof(b));
-		flag = 0;
-		total = 0;
-	}
-	void insert(string s) {
-		int node = 0;
-		FOR (i, sz(s)) {
-			if (ch[node][s[i] - '0'] == -1) ch[node][s[i] - '0'] = ++total;
-			else if (i == sz(s) - 1) flag = 1;
-			node = ch[node][s[i] - '0'];
-			if (b[node]) flag = 1;
-		}
-		b[node] = 1;
-	}
+    void init() {
+        memset(ch, -1, sizeof(ch));
+        memset(b, false, sizeof(b));
+        flag = 0;
+        total = 0;
+    }
+    void insert(string s) {
+        int node = 0;
+        FOR (i, sz(s)) {
+            if (ch[node][s[i] - '0'] == -1) ch[node][s[i] - '0'] = ++total;
+            else if (i == sz(s) - 1) flag = 1;
+            node = ch[node][s[i] - '0'];
+            if (b[node]) flag = 1;
+        }
+        b[node] = 1;
+    }
 } T;
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(0);
-	int t; cin >> t;
-	while (t--) {
-		int tt; cin >> tt;
-		T.init();
-		while (tt--) {
-			string s; cin >> s;
-			T.insert(s);
-		}
-		if (!flag) cout << "YES\n";
-		else cout << "NO\n";
-	}
-	return 0;
+    ios::sync_with_stdio(false); cin.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        int tt; cin >> tt;
+        T.init();
+        while (tt--) {
+            string s; cin >> s;
+            T.insert(s);
+        }
+        if (!flag) cout << "YES\n";
+        else cout << "NO\n";
+    }
+    return 0;
 }
