7cf772a3027bc638245ad2276ce11df7cf577aac
timeflew <136106767+twfly104@users.noreply.github.com>
2024-12-17 23:14:27 +0800

    Update 10051.cpp

--- loj/10051.cpp
+++ loj/10051.cpp
@@ -27,51 +27,51 @@ int a[mxN], ch[mxN * 32 + 1][2], l[mxN + 1], r[mxN + 2];
 
 struct trie {
     int total;
-	void init() {
-		memset(ch, -1, sizeof(ch));
-		total = 0;
-	}
-	void insert(int x) {
-		int idx = 0;
-		for (int i = 31; i >= 0; i--) {
-			int state = (x >> i) & 1;
-			if (ch[idx][state] == -1) {
-				ch[idx][state] = ++total;
-			}
-			idx = ch[idx][state];
-		}
-	}
-	int sum(int x) {
-		int idx = 0, res = 0;
-		for (int i = 31; i >= 0; i--) {
-			int state = (x >> i) & 1;
-			if (~ch[idx][!state]) {
-				idx = ch[idx][!state];
-				res += 1 << i;
-			} else {
-				idx = ch[idx][state];
-			}
-		}
-		return res;
-	}
+    void init() {
+        memset(ch, -1, sizeof(ch));
+        total = 0;
+    }
+    void insert(int x) {
+        int idx = 0;
+        for (int i = 31; i >= 0; i--) {
+            int state = (x >> i) & 1;
+            if (ch[idx][state] == -1) {
+                ch[idx][state] = ++total;
+            }
+            idx = ch[idx][state];
+        }
+    }
+    int sum(int x) {
+        int idx = 0, res = 0;
+        for (int i = 31; i >= 0; i--) {
+            int state = (x >> i) & 1;
+            if (~ch[idx][!state]) {
+                idx = ch[idx][!state];
+                res += 1 << i;
+            } else {
+                idx = ch[idx][state];
+            }
+        }
+        return res;
+    }
 } T;
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(0);
-	int n; cin >> n;
-	FOR (i, n) cin >> a[i];
-	T.init();
-	FOR (i, n) {
-		T.insert(a[i]);
-		l[i + 1] = max(l[i], T.sum(a[i]));
-	}
-	T.init();
-	for (int i = n - 1; i >= 0; i--) {
-		T.insert(a[i]);
-		r[i + 1] = max(r[i + 2], T.sum(a[i]));
-	}
-	int ans = 0;
-	FOR1 (i, 1, n) ans = max(l[i] + r[i], ans);
-	cout << ans;
-	return 0;
+    ios::sync_with_stdio(false); cin.tie(0);
+    int n; cin >> n;
+    FOR (i, n) cin >> a[i];
+    T.init();
+    FOR (i, n) {
+        T.insert(a[i]);
+        l[i + 1] = max(l[i], T.sum(a[i]));
+    }
+    T.init();
+    for (int i = n - 1; i >= 0; i--) {
+        T.insert(a[i]);
+        r[i + 1] = max(r[i + 2], T.sum(a[i]));
+    }
+    int ans = 0;
+    FOR1 (i, 1, n) ans = max(l[i] + r[i], ans);
+    cout << ans;
+    return 0;
 }
