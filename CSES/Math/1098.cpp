38b05a0f810ea0df7d16821ca896a0c4857817f2
timeflew <136106767+twfly104@users.noreply.github.com>
2025-09-03 23:37:38 +0800

    Create 1098.cpp

---
+++ CSES/Math/1098.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x.size())
+ 
+#ifdef debug
+#define dbg(...) cerr<<#__VA_ARGS__<<" = ", de(__VA_ARGS__);
+template<typename T>
+void de(T t) {cerr<<t<<endl;}
+template<typename T, typename ...U>
+void de(T t, U...u) {cerr<<t<<", ", de(u...);}
+#else
+#define dbg(...)
+#define endl "\n"
+#endif
+ 
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t; cin>>t;
+    while(t--) {
+        int n; cin>>n;
+        int res=0;
+        for(int i=0; i<n; i++) {
+            int x; cin>>x;
+            res^=x%4;
+        }
+        if(res) {
+            cout<<"first"<<endl;
+        } else {
+            cout<<"second"<<endl;
+        }
+    }
+    return 0;
+}
+//SG
